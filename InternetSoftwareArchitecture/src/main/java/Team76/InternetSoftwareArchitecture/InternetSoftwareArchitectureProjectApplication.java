3d000553c236832f44f490eb6648b163c0e8470b
lukapetrovic3110 <luka.petrovic@uns.ac.rs>
2021-11-23 17:59:57 +0100

    feat: initialized backend project

---
+++ InternetSoftwareArchitecture/src/main/java/Team76/InternetSoftwareArchitecture/InternetSoftwareArchitectureProjectApplication.java
@@ -0,0 +1,13 @@
+package Team76.InternetSoftwareArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetSoftwareArchitectureProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetSoftwareArchitectureProjectApplication.class, args);
+	}
+
+}
---
+++ InternetSoftwareArchitecture/src/test/java/Team76/InternetSoftwareArchitecture/InternetSoftwareArchitectureProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Team76.InternetSoftwareArchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetSoftwareArchitectureProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
